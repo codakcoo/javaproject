da293f0ac1b06c73478c5f11b50d089791eb8e02
codakcoo <tmdrnjs100@gmail.com>
2026-04-23 15:33:15 +0900

    보안, 회원가입 추가

--- src/main/java/egovframework/member/service/MemberService.java
+++ src/main/java/egovframework/member/service/MemberService.java
@@ -4,4 +4,6 @@ import egovframework.member.vo.MemberVO;
 
 public interface MemberService {
     MemberVO login(String memberId, String password);
+    //인터페이스 매서드 추가
+    void register(MemberVO member);
 }
--- src/main/java/egovframework/member/service/impl/MemberServiceImpl.java
+++ src/main/java/egovframework/member/service/impl/MemberServiceImpl.java
@@ -4,6 +4,7 @@ import egovframework.member.mapper.MemberMapper;
 import egovframework.member.service.MemberService;
 import egovframework.member.vo.MemberVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,13 +13,19 @@ public class MemberServiceImpl implements MemberService {
     @Autowired
     private MemberMapper memberMapper;
 
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    
+    @Override
+    public void register(MemberVO member) {
+        member.setPassword(encoder.encode(member.getPassword())); // BCrypt 암호화
+        memberMapper.insertMember(member);
+    }
+    
     @Override
     public MemberVO login(String memberId, String password) {
-        // DB에서 아이디로 회원 조회
         MemberVO member = memberMapper.selectMember(memberId);
 
-        // 회원이 없거나 비밀번호 불일치 시 null 반환
-        if (member == null || !member.getPassword().equals(password)) {
+        if (member == null || !encoder.matches(password, member.getPassword())) {
             return null;
         }
         return member;
