cf740b0aa0d46dfff264603df2881b20f3902534
codakcoo <tmdrnjs100@gmail.com>
2026-05-01 20:59:16 +0900

    NAS서버 DB연결 완료

--- src/main/java/egovframework/common/DataInitializer.java
+++ src/main/java/egovframework/common/DataInitializer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 
 @Component
-@DependsOn("flyway")
+//@DependsOn("flyway")
 public class DataInitializer {
 
     @Autowired
