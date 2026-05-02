211a8f1ad503068c49495ae77958cb5e48132972
qortmdrnjs <85725677+codakcoo@users.noreply.github.com>
2026-05-02 20:51:03 +0900

    Delete merged_check2 directory

--- merged_check2/src/main/java/egovframework/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package egovframework.member.service;
-
-import egovframework.member.vo.MemberVO;
-
-public interface MemberService {
-    MemberVO login(String memberId, String password);
-    //인터페이스 매서드 추가
-    void register(MemberVO member);
-}
